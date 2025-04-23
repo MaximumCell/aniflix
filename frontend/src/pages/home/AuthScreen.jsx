48bab1564ae3efea55e405c9b36027a6ac32f66c
MaximumCell <alihassanwinner@gmail.com>
2025-04-23 21:12:09 +0500

    auth glitch set

--- frontend/src/pages/home/AuthScreen.jsx
+++ frontend/src/pages/home/AuthScreen.jsx
@@ -10,9 +10,12 @@ const AuthScreen = () => {
 		e.preventDefault();
 		navigate("/signup?email=" + email);
 	};
+	const heroBackgroundStyle = {
+		backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.1)), url('/hero.png')`,
+	};
 
 	return (
-		<div className='hero-bg relative'>
+		<div className='relative' style={heroBackgroundStyle}>
 			{/* Navbar */}
 			<header className='max-w-6xl mx-auto flex items-center justify-between p-4 pb-10'>
 				<img src='/netflix-logo.png' alt='Netflix Logo' className='w-32 md:w-52' />
