d9d0311a0e6c57ce505b80ffa72fca70004abe5b
MaximumCell <alihassanwinner@gmail.com>
2025-04-23 19:11:19 +0500

    deploying 1

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -10,6 +10,9 @@ router.post("/login", login);
 router.post("/logout", logout);
 router.get("/authCheck", protectRoute, authCheck);
 
+router.get("/", (req, res) => {
+    res.send("Hello World! Server is ready");
+})
 
 
 export default router;
